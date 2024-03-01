c02535d6ed43a084d7206ee503386aaf09a6fa22
nemolab24 <159055893+nemolab24@users.noreply.github.com>
2024-03-01 03:14:45 +0100

    The functionality of the website has been added. javaScript file has been completed.
    addEventListener was added to all playabnle buttons.
    As well as the score tracker functionality.

--- javaScript.js
+++ javaScript.js
@@ -1,81 +1,74 @@
-function getComputerChoice(){
-
-  let options = ["Rock", "Paper","Scissor"];
-  let randomOption =  Math.floor(Math.random()*3);
+function getComputerChoice() {
+  let options = ["Rock", "Paper", "Scissor"];
+  let randomOption = Math.floor(Math.random() * 3);
   return options[randomOption];
 }
 
-function playRound(player, computer){
+function playRound(player, computer) {
   player = player.toLowerCase();
   computer = computer.toLowerCase();
-  let options = ["rock", "paper","scissor"];
+  let options = ["rock", "paper", "scissor"];
   player = options.indexOf(player);
   computer = options.indexOf(computer);
-  if (computer == 0){
-    if (player == 0){
-      return("it is an tie, rock vs rock")
+  if (computer == 0) {
+    if (player == 0) {
+      return 0;
+    } else if (player == 1) {
+      return 1;
+    } else {
+      return -1;
     }
-    else if ( player == 1){
-      return("You win, Paper wins Rock")
+  } else if (computer == 1) {
+    if (player == 0) {
+      return -1;
+    } else if (player == 1) {
+      return 0;
+    } else {
+      return 1;
     }
-    else 
-    {
-      return("You lose, Scissors loses to Rock")
-    }
-  }
-  else if (computer == 1){
-    if (player == 0){
-      return("You lose, Rock loses to Paper")
-    }
-    else if ( player == 1){
-      return("it is an tie, paper vs paper")
-    }
-    else 
-    {
-      return("You win, Scissors wins Paper")
-    }
-  }
-  else{
-    if (player == 0){
-      return("You win, Rock wins Scissors")
-    }
-    else if ( player == 1){
-      return("You lose,  Paper loses to Scissors")
-    }
-    else 
-    {
-      return("it is an tie, Scissor vs Scisor")
-    }
-  }
-
-}
-
-
-function playGame(){
-  let scorePlayer = 0;
-  let scoreComputer = 0;
-  for ( let i = 0; i < 5; i++)
-  { let player = prompt("Make your choice: Rock, Paper, Scissor.");
-    let round =  playRound(player,getComputerChoice());
-    if ((round.toLowerCase()).includes("win")){
-      scorePlayer++;
-      console.log(`You won this round, ${scorePlayer} - ${scoreComputer} is the current score. ` )
-    }
-    else if ((round.toLowerCase()).includes("tie")){
-      console.log(`It is a tie, ${scorePlayer} - ${scoreComputer} is the current score. ` )
-    }
-    else{
-      scoreComputer++;
-      console.log(`You Lose this round, ${scorePlayer} - ${scoreComputer} is the current score. ` )
-    }
-    let check = scorePlayer - scoreComputer;
-    if (check>0){
-      console.log("The winner is You, congratulations!")
-    }
-    else {
-      console.log("You have Lost this game, better luck next time.")
+  } else {
+    if (player == 0) {
+      return 1;
+    } else if (player == 1) {
+      return -1;
+    } else {
+      return 0;
     }
   }
 }
 
-playGame();
+const buttons = document.querySelectorAll(".playerSide > button");
+const computerName = document.querySelector(".computerChoice");
+const scoreName = document.querySelector(".score");
+const winnerName = document.querySelector(".winner");
+let counter = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let computer = getComputerChoice();
+    let round = playRound(button.textContent, computer);
+    computerName.textContent = computer;
+    if (round == 1) {
+      playerScore++;
+    } else if (round == -1) {
+      computerScore++;
+    }
+    scoreName.textContent = `${playerScore} - ${computerScore}`;
+    counter++;
+    if (counter >= 5) {
+      if (playerScore > computerScore) {
+        winnerName.textContent = " You have Won the game, Congratulations!";
+      } else {
+        winnerName.textContent =
+          "You have lost the game, better luck next time!";
+      }
+      playerScore = 0;
+      computerScore = 0;
+      counter = 0;
+    } else {
+      winnerName.textContent = "";
+    }
+  });
+});
